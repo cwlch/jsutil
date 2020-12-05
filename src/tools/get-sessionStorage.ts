2e810fbb05c6f2b997171962a7b7a09f28eece2e
蓝方人.ch <cwl_ch@163.com>
2020-12-05 09:07:23 +0800

    1、修改sessionStorage文件名

---
+++ src/tools/get-sessionStorage.ts
@@ -0,0 +1,24 @@
+/*
+ * @Author: ch 
+ * @Date: 2020-11-24 21:38:12 
+ * @Last Modified by: ch
+ * @Last Modified time: 2020-11-10 10:58:35
+ */
+'use strict'
+/**
+ * 
+ * 获取SessionStorage
+ */
+
+const getSessionStorage = (key:string):any => {
+    let val:any = null;
+    if(sessionStorage){
+        sessionStorage.getItem(key);
+    }
+    if(val){
+        val = JSON.parse(val);
+    }
+    return val;
+};
+
+export default getSessionStorage;
