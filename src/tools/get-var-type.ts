22092f19a0f7befa90f4537540216aa67d56a0e7
ch <cwl_ch@163.com>
2022-10-17 09:36:17 +0800

    fix: _getVaType方法增强

--- src/tools/get-var-type.ts
+++ src/tools/get-var-type.ts
@@ -9,17 +9,34 @@
 /**
  * 获取变量类型
  * 'Null',
-    'Undefined',
-    'Object',
-    'Array',
-    'String',
-    'Number',
-    'Boolean',
-    'Function',
-    'RegExp',
-    'NaN',
-    'Infinite'
+ * 'Undefined',
+ * 'Object',
+ * 'Array',
+ * 'String',
+ * 'Number',
+ * 'Boolean',
+ * 'Function',
+ * 'RegExp',
+ * 'NaN',
+ * 'Infinity'
+ * 'Date'
+ * 自定义构造函数类：小写函数名
  * @param {*} o
  * @returns string
  */
-export default (o:any):string => (Object.prototype.toString.call(o).match(/\[object (.*?)\]/) || [])[1].toLowerCase();
+export default (o:any):string =>{
+   let typeStr = (Object.prototype.toString.call(o).match(/\[object (.*?)\]/) || [])[1];
+   if(typeStr === 'Object'){
+      const constName = o.constructor.name
+      constName !== 'Object' && (typeStr = `${typeStr}:${constName}`);
+   } else if(typeStr === 'Number'){
+      if(!isFinite(o)){
+         if(isNaN(o)){
+            typeStr = 'NaN'
+         }else{
+            typeStr = 'Infinity'
+         }
+      }
+   }
+   return typeStr;
+}
